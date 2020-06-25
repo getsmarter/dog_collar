4d4e192b0a7c4788adab0de32f0fcb29ab09b114
James Behr <jbehr@2u.com>
2020-06-25 17:23:39 +0200

    Refactor `before_log` hooks.
    Refactor `DogCollar::Logging::Logger` to use instance variables to store
    a list of `before_log` hooks instead of redefining to
    `DogCollar::Logging::Logger#add` method to include a call to the hook.
    Each class gets its own set of hooks and can inherit the parent class's
    set of hooks. Each instance gets a copy of this list that it can modify
    without affecting other instances.

--- lib/dog_collar/logging/logger.rb
+++ lib/dog_collar/logging/logger.rb
@@ -2,13 +2,10 @@
 
 require 'dog_collar/logging/formatters/json'
 require 'dog_collar/logging/formatters/pretty'
-require 'dog_collar/logging/hooks'
 
 module DogCollar
   module Logging
     class Logger < Logger
-      include Hooks
-
       LOG_SEV = {
         debug: Logger::DEBUG,
         info: Logger::INFO,
@@ -18,6 +15,29 @@ module DogCollar
       }.freeze
 
       attr_accessor :formatter
+
+      def self.before_log_hooks
+        @_before_log_hooks ||= []
+      end
+
+      def before_log_hooks
+        # Grab the hook methods from this class and its ancestors and bind them
+        # to this instance. These hooks will be executed before any
+        # instance-level hooks.
+        @_before_log_hooks ||= inherited_before_log_hooks.map do |hook|
+          hook.bind(self)
+        end
+      end
+
+      def self.before_log(*hooks)
+        hooks.each do |hook|
+          before_log_hooks << instance_method(hook)
+        end
+      end
+
+      def before_log
+        before_log_hooks << Proc.new
+      end
 
       def initialize(*, **)
         super
@@ -46,6 +66,7 @@ module DogCollar
         severity ||= ::Logger::UNKNOWN
         return true if @logdev.nil? || severity < level
 
+        meta = execute_before_log_hooks.update(meta)
         message = evaluate_log_block(message, meta, &block)
 
         @logdev.write(format_message(severity, Time.now, progname, message, meta))
@@ -55,6 +76,27 @@ module DogCollar
       alias log add
 
       private
+
+      def logger_ancestors
+        self.class.ancestors.select { |ancestor| ancestor <= DogCollar::Logging::Logger }
+      end
+
+      def inherited_before_log_hooks
+        logger_ancestors.reverse.flat_map(&:before_log_hooks)
+      end
+
+      def execute_before_log_hooks
+        meta = {}
+        before_log_hooks.each do |hook|
+          case hook
+          when Proc
+            meta.update(instance_eval(&hook))
+          when Method
+            meta.update(hook.call)
+          end
+        end
+        meta
+      end
 
       def evaluate_log_block(message, meta)
         return message unless block_given?
--- lib/dog_collar/version.rb
+++ lib/dog_collar/version.rb
@@ -3,7 +3,7 @@
 module DogCollar
   module VERSION
     MAJOR = 0
-    MINOR = 1
+    MINOR = 2
     PATCH = 0
     PRE = nil
 
