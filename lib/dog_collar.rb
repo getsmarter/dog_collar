1dfb0b7a2b440c477ece959befe209180eaac1e5
Vutivi Xanani <vutivi.xanani@gmail.com>
2021-07-21 12:24:13 +0200

    split dog collar

--- lib/dog_collar.rb
+++ lib/dog_collar.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
-require 'dog_collar/configuration'
-require 'dog_collar/logging/logger'
-require 'dog_collar/logging/instrumented_logger'
-
-module DogCollar
-  extend Configuration
-end
-
+require 'dog_collar/core'
 require 'dog_collar/contrib/sidekiq/integration'
 require 'dog_collar/contrib/rails/integration'
 require 'dog_collar/contrib/circuitry/integration'
