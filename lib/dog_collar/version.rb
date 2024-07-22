af8669e8d8fc7abe5f230de4d96aedf651f9db66
Vutivi <vutivi.xanani@gmail.com>
2024-07-22 13:15:50 +0200

    chore: bump version

--- lib/dog_collar/version.rb
+++ lib/dog_collar/version.rb
@@ -3,8 +3,8 @@
 module DogCollar
   module VERSION
     MAJOR = 0
-    MINOR = 7
-    PATCH = 1
+    MINOR = 8
+    PATCH = 0
     PRE = nil
 
     STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
